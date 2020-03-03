b373bbf6ba798a2cc7370abd8bf2adb61c5b1806
rmogusu <rmogusu123@gmail>
2020-03-03 15:33:10 -0500

    ADD

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,8 +11,18 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-    public static void main(String[] args) {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    public static void main(String[] args) { //type “psvm + tab” to autocreate this
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
+
         String connectionString = "jdbc:postgresql://localhost:5432/todolist"; //connect to todolist, not todolist_test!
         Sql2o sql2o = new Sql2o(connectionString, "rose", "wambua");
         Sql2oTaskDao taskDao = new Sql2oTaskDao(sql2o);
